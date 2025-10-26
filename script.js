2e23ea65a60a8c57c42d15527489d4fbffcb978c
YUANyuyuyu5 <yuandeyy@qq.com>
2025-10-26 09:50:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://darcie-unvitreous-posthypnoticall.ngrok-free.app";
+const API_BASE_URL = "https://darcie-unvitreous-posthypnotically.ngrok-free.dev";
 // 切换页面显示
 function showPage(pageId) {
     document.querySelectorAll('.page').forEach(page => {
